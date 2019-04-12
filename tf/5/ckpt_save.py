e4bcc8c8d6e3f122cb8333e93092e31cd30d229e
broadsun(孙广博) <broadsun@tencent.com>
2019-04-12 18:26:25 +0800

    update

--- tf/5/ckpt_save.py
+++ tf/5/ckpt_save.py
@@ -1,27 +1,25 @@
 import tensorflow as tf
 
-v1 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
-v2 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
-result = v1 + v2
-v3 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
-init_op = tf.global_variables_initializer()
-saver = tf.train.Saver()
+# v1 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
+# v2 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
+# result = v1 + v2
+# v3 = tf.Variable(tf.random_normal([1], stddev=1, seed=1))
+# init_op = tf.global_variables_initializer()
+# saver = tf.train.Saver()
 
-with tf.Session() as sess:
-    sess.run(init_op)
-    saver.save(sess, "save/model.ckpt")
+# with tf.Session() as sess:
+#     sess.run(init_op)
+#     saver.save(sess, "save/model.ckpt")
 
 
-with tf.Session() as sess:
-    saver.restore(sess, "save/model.ckpt")
-    print(sess.run(result))
+# with tf.Session() as sess:
+#     saver.restore(sess, "save/model.ckpt")
+#     print(sess.run(result))
 
 
 saver = tf.train.import_meta_graph("save/model.ckpt.meta")
-
-init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
     saver.restore(sess, "save/model.ckpt")
-    print(sess.run(v1))
-    print(sess.run(v2))
-    print(sess.run(v3))
+    print(sess.run(tf.get_default_graph().get_tensor_by_name("add:0")))
+    # print(sess.run(v2))
+    # print(sess.run(v3))
